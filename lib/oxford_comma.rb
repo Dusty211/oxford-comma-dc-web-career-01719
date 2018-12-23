727849ae83458dba07a0e21acf5737f0973dd642
Kyle Houghton <kylebenz@hotmail.com>
2018-12-23 23:37:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   else
-    array[0..(array.length - 3)].join(", ") + " " + array[(array.length - 2)..(array.length - 1)].join(", and ") 
+    array[0..(array.length - 3)].join(", ") + ", " + array[(array.length - 2)..(array.length - 1)].join(", and ") 
   end
 end
 
