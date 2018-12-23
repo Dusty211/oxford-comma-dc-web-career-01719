72fbbc8cd63aaec83e6303fd2487d694c76780be
Kyle Houghton <kylebenz@hotmail.com>
2018-12-23 23:00:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,14 @@
 def oxford_comma(array)
   case array
-    
+    when length < 2
+      puts "less than two"
+    when length == 2
+      puts "two exactly"
+    when length > 2
+      puts "greater than two"
 end
+
+testarray = [1, 1, 1]
+
+oxford_comma(testarray)
+
